25937d5be5fcdeaa7a9e914b7be15dd5430b5721
saurabh <saurabhkedar@gmail.com>
2022-04-04 05:08:53 +0530

    impleted method to display the expense-income data on the page

--- app.js
+++ app.js
@@ -38,4 +38,17 @@ init()
 //Update Data
 function updateValue() {
   const amounts = transactions.map((transaction) => transaction.amount)
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2)
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2)
+
+  balance.innerText = `${total} ₹`
+  moneyPlus.innerText = `${income} ₹`
+  moneyMinus.innerText = `${expense} ₹`
 }
